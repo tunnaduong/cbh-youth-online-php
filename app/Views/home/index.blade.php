41f2cc7b1ff415198c4c5473e8b6f514978465ac
Dương Tùng Anh <tunnaduong@gmail.com>
2024-11-15 20:02:01 +0700

    Enhance post display: update card styling, adjust padding, and improve element alignment

--- app/Views/home/index.blade.php
+++ app/Views/home/index.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
     <div class="flex flex-col items-center w-full flex-1 p-2 pt-3">
-        <div data-post-id="213012" class="w-full mb-5 shadow-lg rounded-lg !p-4 bg-white flex flex-row md:max-w-[679px]">
-            <div class="min-w-[84px] items-center mt-1 flex-col flex ml-[-15px] text-[13px] font-semibold text-gray-400">
+        <div data-post-id="213012" class="w-full mb-5 shadow-lg rounded-xl !p-6 bg-white flex flex-row md:max-w-[679px]">
+            <div class="min-w-[84px] items-center mt-1 flex-col flex ml-[-20px] text-[13px] font-semibold text-gray-400">
                 <ion-icon name="arrow-up-outline" class="text-3xl cursor-pointer"></ion-icon>
                 <span class="select-none text-lg">3</span>
                 <ion-icon name="arrow-down-outline" class="text-3xl cursor-pointer"></ion-icon>
                 <div
                     class="bg-[#EAEAEA] cursor-pointer rounded-lg w-[33.6px] h-[33.6px] mt-3 flex items-center justify-center">
-                    <ion-icon name="bookmark" class="text-[#CACACA] text-xl"></ion-icon>
+                    <ion-icon name="bookmark" class="text-gray-400 text-xl"></ion-icon>
                 </div>
             </div>
             <div class="flex-1 overflow-hidden break-words"><a href="/tunnaduong/posts/213012">
@@ -29,7 +29,7 @@
                         class="object-contain max-h-96 text-[11px]"
                         src="https://api.chuyenbienhoa.com/storage/images/1730457490_image.jpg" style="color: transparent;">
                 </div>
-                <hr class="my-3 border-t-2">
+                <hr class="!my-5 border-t-2">
                 <div class="flex-row flex text-[13px] items-center"><a href="/tunnaduong"><span
                             class="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
                             <img class="aspect-square h-full w-full" alt="tunnaduong avatar"
@@ -37,7 +37,7 @@
                         </span>
                     </a>
                     <span class="text-gray-500 hidden md:block ml-2">Đăng bởi</span>
-                    <a class="ml-1.5 md:ml-0.5 text-[#319527] hover:text-[#319527] font-bold" href="/tunnaduong">Tùng A
+                    <a class="ml-2 md:ml-1 text-[#319527] hover:text-[#319527] font-bold" href="/tunnaduong">Tùng A
                         Dính</a>
                     <span class="mb-2 ml-0.5 text-sm text-gray-500">.</span>
                     <span class="ml-0.5 text-gray-500">1
