1329fcba173d58172d8ba48706cf8c58bd7ba071
Dương Tùng Anh <tunnaduong@gmail.com>
2024-11-26 21:23:15 +0700

    Enhance 404 error page styling and adjust zoom level for better visibility

--- app/Views/errors/404.blade.php
+++ app/Views/errors/404.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.error')
 
 @section('content')
-    <div class="flex flex-1 items-center justify-center"
+    <div class="flex flex-1 items-center justify-center" id="error-404"
         style="zoom:1.4;display:block;position:absolute;left:50%;top:50%;-webkit-transform:translate(-50%, -50%);transform:translate(-50%, -50%)">
         <center><img src="/assets/images/404.svg" alt="404" class="w-[80px] h-[80px] mb-2">
             <h4 class="font-bold text-gray-500 text-[14px]">Bạn hiện không xem được nội dung này</h4>
-            <p class="text-[12px] text-gray-500">Lỗi này thường do chủ sở hữu chỉ chia sẻ nội dung với một<br>nhóm nhỏ, thay
+            <p class="text-[12px] text-gray-500 max-w-[350px]">Lỗi này thường do chủ sở hữu chỉ chia sẻ nội dung với một nhóm
+                nhỏ,
+                thay
                 đổi người được xem hoặc đã xóa nội dung.</p>
             <button onclick="window.location.href='/'"
                 class="inline-flex items-center justify-center gap-2 whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&amp;_svg]:pointer-events-none [&amp;_svg]:size-4 [&amp;_svg]:shrink-0 shadow bg-[#319528] hover:bg-green-700 text-white text-[12px] font-semibold rounded-[5px] py-[5px] px-6 mt-3 h-7">Đi
