625154798c79add45f0fa9214699f30bcdda0f16
Duong Tung Anh <tunnaduong@gmail.com>
2024-11-27 11:45:09 +0700

    Add Follow model with follow, unfollow, and isFollowing methods

---
+++ app/Models/Follow.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+class Follow extends BaseModel
+{
+    public function follow($followerId, $followedId)
+    {
+        $this->setQuery("INSERT INTO cyo_user_followers (follower_id, followed_id, created_at) VALUES (?, ?, ?, ?)");
+        return $this->execute([$followerId, $followedId, date('Y-m-d H:i:s')]);
+    }
+
+    public function unfollow($followerId, $followedId)
+    {
+        $this->setQuery("DELETE FROM cyo_user_followers WHERE follower_id = ? AND followed_id = ?");
+        return $this->execute([$followerId, $followedId]);
+    }
+
+    public function isFollowing($followerId, $followedId)
+    {
+        $this->setQuery("SELECT * FROM cyo_user_followers WHERE follower_id = ? AND followed_id = ?");
+        return $this->loadRow([$followerId, $followedId]);
+    }
+}
