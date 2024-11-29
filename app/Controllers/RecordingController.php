3f1a47035170c530944cad0557a85edd07191758
Duong Tung Anh <tunnaduong@gmail.com>
2024-11-29 16:58:05 +0700

    Implement recording model and controller; update recording view to display dynamic recordings list with user details

--- app/Controllers/RecordingController.php
+++ app/Controllers/RecordingController.php
@@ -2,12 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Models\Recording;
 use App\Controllers\BaseController;
 
 class RecordingController extends BaseController
 {
+    protected $recordingModel;
+
+    public function __construct()
+    {
+        $this->recordingModel = new Recording();
+    }
+
     public function index()
     {
-        return $this->render('recording.index');
+        $recordings = $this->recordingModel->getRecordings();
+
+        return $this->render('recording.index', [
+            'recordings' => $recordings,
+        ]);
     }
 }
