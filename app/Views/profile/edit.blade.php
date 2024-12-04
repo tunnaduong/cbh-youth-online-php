c9937494a6a90197062b055e2e8a218c69d711a1
Duong Tung Anh <tunnaduong@gmail.com>
2024-12-04 13:11:45 +0700

    Enhance profile edit page with improved layout, accessibility, and localized text

--- app/Views/profile/edit.blade.php
+++ app/Views/profile/edit.blade.php
@@ -11,18 +11,18 @@
                 <p class="text-muted-foreground">Quản lý cài đặt tài khoản và thiết lập thông báo email.</p>
             </div>
             <div class="my-6 border-top"></div>
-            <div class="flex gap-x-12">
-                <aside class="w-1/5">
+            <div class="flex flex-col lg:flex-row gap-x-12">
+                <aside class="w-full lg:w-1/5 mb-4">
                     <ul class="uk-nav uk-nav-primary" uk-switcher="connect: #component-nav; animation: uk-animation-fade"
                         uk-nav>
                         <li class="uk-active">
-                            <a href="#">Trang cá nhân</a>
+                            <a href="#" class="mx-0">Trang cá nhân</a>
                         </li>
                         <li>
-                            <a href="#">Tài khoản</a>
+                            <a href="#" class="mx-0">Tài khoản</a>
                         </li>
                         <li>
-                            <a href="#">Thông báo</a>
+                            <a href="#" class="mx-0">Thông báo</a>
                         </li>
                     </ul>
                 </aside>
@@ -87,7 +87,8 @@
                             <div class="space-y-2">
                                 <label class="uk-form-label block" for="date_of_birth">Ngày tháng năm sinh</label>
                                 <input class="uk-input w-[240px]" id="date_of_birth" type="date"
-                                    placeholder="Pick a date">
+                                    placeholder="Chọn một ngày" value="{{ $profile->birthday }}" min="1900-01-01"
+                                    max="{{ date('Y-m-d') }}">
                                 <div class="uk-form-help text-muted-foreground">Ngày sinh của bạn được sử dụng để tính toán
                                     tuổi của bạn.
                                 </div>
@@ -110,87 +111,73 @@
                         <li class="space-y-6">
                             <div>
                                 <h3 class="text-lg font-medium">Thông báo</h3>
-                                <p class="text-sm text-muted-foreground"> Cấu hình cách bạn nhận thông báo. </p>
+                                <p class="text-sm text-muted-foreground">Cấu hình cách bạn nhận thông báo.</p>
                             </div>
                             <div class="border-t border-border"></div>
                             <div class="space-y-2">
                                 <span class="uk-form-label">Thông báo cho tôi về</span>
-                                <label class="block text-sm" for="notification_0">
-                                    <input id="notification_0" class="uk-radio mr-2" name="notification" type="radio">Tất
-                                    cả thông báo</label>
-                                <label class="block text-sm" for="notification_1">
-                                    <input id="notification_1" class="uk-radio mr-2" name="notification" type="radio">
-                                    Tin nhắn trực tiếp và đề cập </label>
-                                <label class="block text-sm" for="notification_2">
-                                    <input id="notification_2" class="uk-radio mr-2" name="notification" type="radio"
-                                        checked="true"> Không có thông báo </label>
+                                <div class="uk-form-controls">
+                                    <label class="flex items-center text-sm" for="notification_0">
+                                        <input id="notification_0" class="mr-2" name="notification" type="radio"
+                                            checked="true">Tất
+                                        cả thông báo</label>
+                                    <label class="flex items-center text-sm" for="notification_1">
+                                        <input id="notification_1" class="mr-2" name="notification" type="radio">Tin
+                                        nhắn trực tiếp và đề cập</label>
+                                    <label class="flex items-center text-sm" for="notification_2">
+                                        <input id="notification_2" class="mr-2" name="notification"
+                                            type="radio">Không có thông báo</label>
+                                </div>
                             </div>
                             <div>
-                                <h3 class="mb-4 text-lg font-medium">Email Notifications</h3>
+                                <h3 class="mb-4 text-lg font-medium">Thông báo qua email</h3>
                                 <div class="space-y-4">
                                     <div class="flex items-center justify-between rounded-lg border border-border p-4">
                                         <div class="space-y-0.5">
-                                            <label class="text-base font-medium" for="email_notification_0"> Communication
-                                                emails
+                                            <label class="text-base font-medium" for="email_notification_0"> Email liên
+                                                lạc
                                             </label>
-                                            <div class="uk-form-help text-muted-foreground"> Receive emails about your
-                                                account
-                                                activity. </div>
+                                            <div class="uk-form-help text-muted-foreground"> Nhận email về hoạt động tài
+                                                khoản của bạn. </div>
                                         </div>
-                                        <input class="uk-toggle-switch uk-toggle-switch-primary" id="email_notification_0"
-                                            type="checkbox">
+                                        <input class="uk-toggle-switch uk-toggle-switch-primary shrink-0 ml-2"
+                                            id="email_notification_0" type="checkbox" checked="true">
                                     </div>
                                     <div class="flex items-center justify-between rounded-lg border border-border p-4">
                                         <div class="space-y-0.5">
-                                            <label class="text-base font-medium" for="email_notification_1"> Marketing
-                                                emails
+                                            <label class="text-base font-medium" for="email_notification_1">Email
+                                                marketing
                                             </label>
-                                            <div class="uk-form-help text-muted-foreground"> Receive emails about new
-                                                products,
-                                                features, and more. </div>
+                                            <div class="uk-form-help text-muted-foreground"> Nhận email về sản phẩm mới,
+                                                tính năng, và nhiều hơn nữa. </div>
                                         </div>
-                                        <input class="uk-toggle-switch uk-toggle-switch-primary" id="email_notification_1"
-                                            type="checkbox">
+                                        <input class="uk-toggle-switch uk-toggle-switch-primary shrink-0 ml-2"
+                                            id="email_notification_1" type="checkbox">
                                     </div>
                                     <div class="flex items-center justify-between rounded-lg border border-border p-4">
                                         <div class="space-y-0.5">
-                                            <label class="text-base font-medium" for="email_notification_2"> Social emails
+                                            <label class="text-base font-medium" for="email_notification_2"> Email xã hội
                                             </label>
-                                            <div class="uk-form-help text-muted-foreground"> Receive emails for friend
-                                                requests,
-                                                follows, and more. </div>
+                                            <div class="uk-form-help text-muted-foreground"> Nhận email về yêu cầu kết bạn,
+                                                theo dõi, và nhiều hơn nữa. </div>
                                         </div>
-                                        <input class="uk-toggle-switch uk-toggle-switch-primary" id="email_notification_2"
-                                            type="checkbox" checked="true">
+                                        <input class="uk-toggle-switch uk-toggle-switch-primary shrink-0 ml-2"
+                                            id="email_notification_2" type="checkbox" checked="true">
                                     </div>
                                     <div class="flex items-center justify-between rounded-lg border border-border p-4">
                                         <div class="space-y-0.5">
-                                            <label class="text-base font-medium" for="email_notification_3"> Security
-                                                emails
+                                            <label class="text-base font-medium" for="email_notification_3"> Email bảo mật
                                             </label>
-                                            <div class="uk-form-help text-muted-foreground"> Receive emails about your
-                                                account
-                                                activity and security. </div>
+                                            <div class="uk-form-help text-muted-foreground"> Nhận email về hoạt động và bảo
+                                                mật tài khoản của bạn. </div>
                                         </div>
-                                        <input class="uk-toggle-switch uk-toggle-switch-primary" id="email_notification_3"
-                                            type="checkbox" checked="true" disabled>
+                                        <input class="uk-toggle-switch uk-toggle-switch-primary shrink-0 ml-2"
+                                            id="email_notification_3" type="checkbox" checked="true" disabled>
                                     </div>
                                 </div>
                             </div>
-                            <div class="flex gap-x-3">
-                                <input class="uk-checkbox mt-1" id="notification_mobile" type="checkbox" checked>
-                                <div class="space-y-1">
-                                    <label class="uk-form-label" for="notification_mobile"> Use different settings for my
-                                        mobile
-                                        devices </label>
-                                    <div class="uk-form-help text-muted-foreground"> You can manage your mobile
-                                        notifications
-                                        in
-                                        the mobile settings page. </div>
-                                </div>
-                            </div>
-                            <div class="">
-                                <button class="uk-button uk-button-primary"> Update notifications
+                            <div>
+                                <button class="uk-button uk-button-primary"> Cập nhật thông báo
                                 </button>
                             </div>
                         </li>
