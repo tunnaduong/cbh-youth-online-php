2141290e00b1dafb3bc6776c23f37ac09d3c1323
Dương Tùng Anh <tunnaduong@gmail.com>
2025-04-22 19:31:26 +0700

    Refactor left sidebar layout and add settings, help, and feedback links

--- app/Views/includes/leftSidebar.blade.php
+++ app/Views/includes/leftSidebar.blade.php
@@ -1,5 +1,5 @@
 <!-- Left side bar -->
-<div class="w-[260px] hidden xl:block !p-6 h-min sticky top-[69px]" id="left-sidebar">
+<div class="w-[260px] hidden xl:flex flex-col !p-6 sticky top-[69px] h-[calc(100vh-69px)]" id="left-sidebar">
     <p class="text-sm font-semibold text-[#6b6b6b] pb-3 ml-2.5">
         MENU
     </p>
@@ -125,4 +125,61 @@
             ])>Đã lưu</div>
         </a>
     @endif
+    {{-- Bottom bar, including settings, help, feedback --}}
+    <div class="flex-1 flex flex-col justify-end">
+        <hr class="mb-3">
+        @if (isset($_SESSION['user']))
+            <a href="/{{ $_SESSION['user']->username }}/settings" @class([
+                'mb-3 text-base font-semibold flex items-center w-[100%] text-left rounded-xl p-2.5',
+                'hover:text-[#319527] text-[#319527] bg-[#E4EEE3]' => $settings ?? false,
+                'text-[#CACACA] hover:text-[#CACACA]' => !($settings ?? false),
+            ])>
+                <div @class([
+                    'text-lg rounded-lg w-[30px] h-[30px] mr-3 menu-border flex items-center justify-center',
+                    '!border-[#BFE5BB] bg-[#CDEBCA]' => $settings ?? false,
+                    'border-[#ECECEC]' => !($settings ?? false),
+                ])>
+                    <ion-icon name="settings"></ion-icon>
+                </div>
+                <div @class([
+                    'text-[#319527]' => $settings ?? false,
+                    'text-[#6B6B6B]' => !($settings ?? false),
+                ])>Cài đặt</div>
+            </a>
+        @endif
+        <a href="/help" @class([
+            'mb-3 text-base font-semibold flex items-center w-[100%] text-left rounded-xl p-2.5',
+            'hover:text-[#319527] text-[#319527] bg-[#E4EEE3]' => $help ?? false,
+            'text-[#CACACA] hover:text-[#CACACA]' => !($help ?? false),
+        ])>
+            <div @class([
+                'text-lg rounded-lg w-[30px] h-[30px] mr-3 menu-border flex items-center justify-center',
+                '!border-[#BFE5BB] bg-[#CDEBCA]' => $help ?? false,
+                'border-[#ECECEC]' => !($help ?? false),
+            ])>
+                <ion-icon name="help-circle"></ion-icon>
+            </div>
+            <div @class([
+                'text-[#319527]' => $help ?? false,
+                'text-[#6B6B6B]' => !($help ?? false),
+            ])>Trợ giúp</div>
+        </a>
+        <a href="https://forms.gle/XJ3v1vN82BxLUVWo9" @class([
+            'text-base font-semibold flex items-center w-[100%] text-left rounded-xl p-2.5',
+            'hover:text-[#319527] text-[#319527] bg-[#E4EEE3]' => $feedback ?? false,
+            'text-[#CACACA] hover:text-[#CACACA]' => !($feedback ?? false),
+        ]) target="_blank">
+            <div @class([
+                'text-lg rounded-lg w-[30px] h-[30px] mr-3 menu-border flex items-center justify-center',
+                '!border-[#BFE5BB] bg-[#CDEBCA]' => $feedback ?? false,
+                'border-[#ECECEC]' => !($feedback ?? false),
+            ])>
+                <ion-icon name="chatbubbles"></ion-icon>
+            </div>
+            <div @class([
+                'text-[#319527]' => $feedback ?? false,
+                'text-[#6B6B6B]' => !($feedback ?? false),
+            ])>Góp ý</div>
+        </a>
+    </div>
 </div>
