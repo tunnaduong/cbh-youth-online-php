aee7318206852be6b534301196b2281125058d4e
Dương Tùng Anh <tunnaduong@gmail.com>
2025-04-23 14:51:09 +0700

    update footer

--- app/Views/includes/footer.blade.php
+++ app/Views/includes/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
-                <p>&copy; {{ date('Y') }} Your Company Name. All rights reserved.</p>
+                <p>&copy; {{ date('Y') }} Công ty TNHH <a href="https://fatties.vn">Fatties Software</a> – Được phát triển bởi học sinh, dành cho học sinh.</p>
             </div>
         </div>
     </div>
