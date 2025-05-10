2cf504a40acb959b1c3b98fcf8e0337f87729887
Dương Tùng Anh <tunnaduong@gmail.com>
2025-05-10 07:13:27 +0700

    Update head.blade.php

--- app/Views/includes/head.blade.php
+++ app/Views/includes/head.blade.php
@@ -1,3 +1,12 @@
+<!-- Bộ mã Google Analytics tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MYK6XE8MX3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MYK6XE8MX3');
+</script>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <title>{{ !empty($title) && $title != 'Diễn đàn' ? $title . ' - ' : '' }}Diễn đàn học sinh Chuyên Biên Hòa</title>
